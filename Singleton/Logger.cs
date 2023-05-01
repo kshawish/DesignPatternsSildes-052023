aa75ee2d246623ba60d01913759a3bc298cf348b
AsiaShalaldeh <arwwd2002@gmail.com>
2023-05-01 16:45:02 +0300

    Add Singleton Code and Slides

---
+++ Singleton/Logger.cs
@@ -0,0 +1,30 @@
+﻿namespace SingletonPattern
+{
+    public sealed class Logger
+    {
+        private static Logger _logger;
+        private static readonly object _syncLock = new object();
+        private Logger()
+        {
+
+        }
+        public static Logger GetLogger()
+        {
+            if (_logger == null)
+            {
+                lock (_syncLock)
+                {
+                    if (_logger == null)
+                    {
+                        _logger = new Logger();
+                    }
+                }
+            }
+            return _logger;
+        }
+        public void WriteMessage(string message)
+        {
+
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SingletonPattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Client.DoTask();
+
+
+            // As you can see, the Singleton pattern allows us to ensure that only
+            // one instance of a class is created and provides a globally accessible
+            // point of access to that instance.
+
+            //Singleton obj1 = Singleton.GetInstance;
+            //Singleton obj2 = Singleton.GetInstance;
+            //Console.WriteLine("obj1 hash code: " + obj1.GetHashCode());
+            //Console.WriteLine("obj2 hash code: " + obj2.GetHashCode());
+
+            Parallel.Invoke(() =>
+            {
+                Singleton obj1 = Singleton.GetInstance;
+                Console.WriteLine("obj1 hash code: " + obj1.GetHashCode());
+            },
+            () =>
+            {
+                Singleton obj1 = Singleton.GetInstance;
+                Console.WriteLine("obj1 hash code: " + obj1.GetHashCode());
+            });
+
+        }
+    }
+}
